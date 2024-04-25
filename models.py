ae22a74a8e60b2e32ac83e14fbca65067a0799f1
tomcjackc <tomcjackc@gmail.com>
2024-04-25 18:48:06 +0100

    reimplement burgers, begin extending to darcy

--- models.py
+++ models.py
@@ -8,26 +8,36 @@ from sklearn.metrics import r2_score
 import gpjax as gpx
 from jax import jit
 import jax.numpy as jnp
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import Matern, RBF
+from tqdm import tqdm
+import multiprocessing as mp
+from functools import partial
 
 class first_model():
     def __init__(self, low_dim_x, low_dim_y = 1, low_dim_regressor = 'linear', GP_params = None, multiinput = False):
         self.low_dim_x = low_dim_x
         self.low_dim_y = low_dim_y
         self.PCA_model_x = PCA(n_components=low_dim_x)
-        self.PCA_model_y = PCA(n_components=low_dim_y)
+        if low_dim_y is not None:
+            self.PCA_model_y = PCA(n_components=low_dim_y)
 
         self.low_dim_regressor_name = low_dim_regressor
         self.GP_params = GP_params
         self.multiinput = multiinput
-        self.has_history = False
+        self.has_train_history = False
 
     def fit(self, X_train, Y_train, save = False, return_bounds: bool | float | int = False):
         self.n_samples_train = X_train.shape[0]
         self.n_features = X_train.shape[1]
         self.n_targets = Y_train.shape[1]
+        self.low_dim_y = self.n_features if self.low_dim_y is None else self.low_dim_y
 
         self.X_train_low_dim = self.PCA_model_x.fit_transform(X_train)
-        self.Y_train_low_dim = self.PCA_model_y.fit_transform(Y_train)
+        if self.low_dim_y != self.n_features:
+            self.Y_train_low_dim = self.PCA_model_y.fit_transform(Y_train)
+        else:
+            self.Y_train_low_dim = Y_train
         print(self.X_train_low_dim.shape)
         print(self.Y_train_low_dim.shape)
 
@@ -37,11 +47,14 @@ class first_model():
             self.low_dim_regressor_list = [LinearRegression() for i in range(self.low_dim_y)]
         elif self.low_dim_regressor_name == 'GP':
             self.low_dim_regressor_list = [GP_regressor(self.GP_params) for i in range(self.low_dim_y)]
+        elif self.low_dim_regressor_name == 'skl_GP':
+            self.low_dim_regressor_list = [GaussianProcessRegressor(kernel = RBF(), alpha = 1e-10,  normalize_y = True, random_state= 1172023) for i in range(self.low_dim_y)]
         else:
-            raise ValueError("Invalid regressor type. Must be 'linear' or 'GP'")
+            raise ValueError("Invalid regressor type. Must be 'linear', 'GP', or 'skl_GP'")
         
         if self.multiinput:
             for i, regressor in enumerate(self.low_dim_regressor_list):
+                print(self.X_train_low_dim.shape, self.Y_train_low_dim[:, i].shape)
                 regressor.fit(self.X_train_low_dim, self.Y_train_low_dim[:, i])
         else:
             for i, regressor in enumerate(self.low_dim_regressor_list):
@@ -55,19 +68,23 @@ class first_model():
             self.Y_train_low_dim_pred_lower = np.zeros((self.n_samples_train, self.low_dim_y))
             if self.multiinput:
                 for i in range(self.low_dim_y):
-                    mean_and_bounds = self.low_dim_regressor_list[i].predict(self.X_train_low_dim, return_bounds)
+                    mean_and_bounds = self.low_dim_regressor_list[i].predict(self.X_train_low_dim)
                     self.Y_train_low_dim_pred[:,i], self.Y_train_low_dim_pred_upper[:,i], self.Y_train_low_dim_pred_lower[:,i] = mean_and_bounds[0], mean_and_bounds[1], mean_and_bounds[2]
             else:
                 for i in range(self.low_dim_y):
-                    mean_and_bounds = self.low_dim_regressor_list[i].predict(self.X_train_low_dim[:,i].reshape(-1,1), return_bounds)
+                    mean_and_bounds = self.low_dim_regressor_list[i].predict(self.X_train_low_dim[:,i].reshape(-1,1))
                     self.Y_train_low_dim_pred[:,i], self.Y_train_low_dim_pred_upper[:,i], self.Y_train_low_dim_pred_lower[:,i] = mean_and_bounds[0], mean_and_bounds[1], mean_and_bounds[2]
             print(self.Y_train_low_dim_pred.shape)
-            self.Y_train_pred = self.PCA_model_y.inverse_transform(self.Y_train_low_dim_pred)
+            if self.low_dim_y == self.n_features:
+                self.Y_train_pred = self.Y_train_low_dim_pred
+            else:
+                self.Y_train_pred = self.PCA_model_y.inverse_transform(self.Y_train_low_dim_pred)
+                
             self.train_rmse = np.sqrt(np.mean((Y_train - self.Y_train_pred) ** 2, axis=1))
-            self.has_history = True
+            self.has_train_history = True
             
     
-    def predict(self, X, return_std = False, return_bounds: bool | float | int = False):
+    def predict(self, X, save = False, return_bounds: bool | float | int = False):
         if self.n_features != X.shape[1]:
             raise ValueError("Input dimension mismatch")
         X_low_dim = self.PCA_model_x.transform(X)
@@ -83,6 +100,13 @@ class first_model():
         Y_pred = self.PCA_model_y.inverse_transform(Y_low_dim_pred)
         Y_pred_upper = self.PCA_model_y.inverse_transform(Y_low_dim_pred_upper)
         Y_pred_lower = self.PCA_model_y.inverse_transform(Y_low_dim_pred_lower)
+
+        if save:
+            self.X_test_low_dim = X_low_dim
+            self.Y_test_low_dim_pred = Y_low_dim_pred
+            self.Y_test_low_dim_pred_upper = Y_low_dim_pred_upper
+            self.Y_test_low_dim_pred_lower = Y_low_dim_pred_lower
+
         if return_bounds is False:
             return Y_pred
         else:
@@ -120,8 +144,8 @@ class GP_regressor():
             negative_mll = jit(negative_mll)
             # hyperparam tuning
             self.opt_posterior, self.history = gpx.fit_scipy(model=posterior, objective=negative_mll, train_data=self.D, max_iters=1000)
-            print(dir(self.opt_posterior))
-            print(self.history)
+            # print(dir(self.opt_posterior))
+            # print(self.history)
             print(self.opt_posterior.prior)
             print(self.opt_posterior.likelihood)
         else:
@@ -142,6 +166,7 @@ class GP_regressor():
 
         predictive_mean = predictive_dist.mean()
         predictive_std = predictive_dist.stddev()
+        print(predictive_std)
         if return_bounds is True:
             lower_bound = predictive_mean - 2 * predictive_std
             upper_bound = predictive_mean + 2 * predictive_std
@@ -153,3 +178,54 @@ class GP_regressor():
             upper_bound = predictive_mean + return_bounds * predictive_std
             return predictive_mean, lower_bound, upper_bound
         
+class second_model():
+    def __init__(self, x_low_dim):
+        self.n_pca = x_low_dim
+        return None
+    
+    # def fit(self, x_train, y_train, model):
+    #     self.x_pca = PCA(n_components=self.n_pca)
+    #     self.x_train_pca = self.x_pca.fit_transform(x_train)
+    #     self.models = []
+    #     for i in tqdm(range(y_train.shape[-1])):
+    #         model.fit(self.x_train_pca, y_train[:, i])
+    #         self.models.append(model)
+    #     print(self.models)
+    #     return None
+
+    # def pred(self, x_test):
+    #     x_test_pca = self.x_pca.transform(x_test)
+    #     pred = []
+    #     for i in tqdm(range(x_test.shape[-1])):
+    #         pred.append(self.models[i].predict(x_test_pca))
+    #         # print(self.model.kernel_)
+    #     #pred_train = gp.predict(x_train)
+
+    #     return np.stack(pred).T
+    
+    def train_test(self, x_train, x_test, y_train, model, parallel = False, verbose = False):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.x_pca = PCA(n_components=self.n_pca)
+        self.x_train_pca = self.x_pca.fit_transform(x_train)
+        self.x_test_pca = self.x_pca.transform(x_test)
+
+        if parallel:
+            pool = mp.Pool(mp.cpu_count())
+            f = partial(fit_predict, model = model, x_train_pca = self.x_train_pca, y_train = y_train, x_test_pca = self.x_test_pca, verbose = verbose)
+            pred = pool.map(f, range(y_train.shape[-1]))
+        else:
+            pred = []
+            for i in tqdm(range(y_train.shape[-1])):
+                model.fit(self.x_train_pca, y_train[:, i])
+                pred.append(model.predict(self.x_test_pca, return_std = True))
+
+
+        return np.stack(pred).T
+    
+def fit_predict(i, model, x_train_pca, y_train, x_test_pca, verbose):
+    if verbose:
+        print(model.kernel_)
+    model.fit(x_train_pca, y_train[:, i])
+    return model.predict(x_test_pca, return_std = True)
